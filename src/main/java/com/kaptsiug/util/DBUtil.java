a060c0f41b31ed8599814135bc82433581ecb2ae
Heorhi <heorhi.kaptsiug@gmail.com>
2021-03-10 22:44:34 +0300

    initial commit

--- src/main/java/com/kaptsiug/util/DBUtil.java
+++ src/main/java/com/kaptsiug/util/DBUtil.java
@@ -16,16 +16,14 @@ public class DBUtil {
     public static Connection getConnection() {
         try {
             Class.forName(JDBC_DRIVER);
-//                Properties prop = new Properties();
-//                InputStream inputStream = DBUtil.class.getClassLoader()
-//                        .getResourceAsStream(PROPERTIES_FILE);
-//                prop.load(inputStream);
-//                String url = prop.getProperty("url");
-//                String user = prop.getProperty("user");
-//                String password = prop.getProperty("password");
-//                connection = DriverManager.getConnection(url, user, password);
-            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/http", "root", "root");
-        } catch (SQLException | ClassNotFoundException e) {
+            Properties prop = new Properties();
+            InputStream inputStream = DBUtil.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE);
+            prop.load(inputStream);
+            String url = prop.getProperty("url");
+            String user = prop.getProperty("user");
+            String password = prop.getProperty("password");
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException | ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
         return connection;
